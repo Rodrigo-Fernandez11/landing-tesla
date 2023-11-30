7e2db7805306ed3352a97ad33abe6865cd953359
Rodrigo-Fernandez11 <101433818+Rodrigo-Fernandez11@users.noreply.github.com>
2023-11-29 23:49:21 -0300

    correction buttons

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -18,26 +18,27 @@ export const LandingSection: React.FC<SectionProps> = ({
   return (
     <section className={`bg-${color} h-screen w-screen text-center relative overflow-hidden`}>
       <div className="z-30 relative h-full flex flex-col">
-        <header>
-          <h2 className={`text-${textColor} pt-40 text-3xl font-bold`}>
+        <header className="pt-20">
+          {/* Cambiamos pt-40 a pt-30 para reducir el espacio superior */}
+          <h2 className={`text-${textColor} text-3xl font-bold`}>
             {title}
           </h2>
           <p className={`${textColor} text-sm`}>
             {subtitle}
           </p>
         </header>
-        <footer className="flex flex-col flex-grow justify-end pb-20 space-y-4">
-          {/* Añadimos la clase "space-y-4" al contenedor para separar los botones */}
+        <footer className="flex flex-col flex-grow justify-end pb-20">
+          {/* Utilizamos la clase "space-y-4" en el contenedor para separar los botones */}
           <div>
             {/* Estilizamos los botones */}
             <a
-              className={`text-${textColor} border-${textColor} border-2 bg-white/5 backdrop-blur-sm text-sm rounded font-medium px-12 py-2 inline-block transition-colors hover:bg-opacity-50 hover:text-white`}
+              className={`text-${textColor} border-${color} border-2  backdrop-blur-sm text-sm rounded font-medium px-12 py-2 mx-2 inline-block transition-colors hover:bg-opacity-20 hover:bg-gray-800 `}
               href="#"
             >
               Explorar inventario
             </a>
             <a
-              className={`text-${textColor} border-${textColor} border-2 bg-white/5 backdrop-blur-sm text-sm rounded font-medium px-12 py-2 inline-block transition-colors hover:bg-opacity-50 hover:text-white`}
+              className={`text-${textColor} border-${color} border-2  backdrop-blur-sm text-sm rounded font-medium px-12 py-2 mx-2 inline-block transition-colors hover:bg-opacity-20 hover:bg-gray-800`}
               href="#"
             >
               Pedir personalizado
